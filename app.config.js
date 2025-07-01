1cf53de2cb5cb3110929abd745447a92a2fb90ab
Will Dennis <williamldennis@gmail.com>
2025-07-01 13:23:39 -0400

    trying to build

--- app.config.js
+++ app.config.js
@@ -1,29 +1,29 @@
 import 'dotenv/config';
 
 export default ({ config }) => ({
-  ...config,
-  name: "fwf",
-  slug: "fwf",
-  version: '1.0.0',
-  extra: {
-    supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
-    supabaseKey: process.env.EXPO_PUBLIC_SUPABASE_KEY,
-  },
-  ios: {
-    ...config.ios,
-    bundleIdentifier: "com.willydennis.fwf",
-    supportsTablet: true,
-    infoPlist: {
-      NSContactsUsageDescription: "This app needs access to your contacts to help you connect with friends.",
-      NSLocationWhenInUseUsageDescription: "This app needs your location to show you the weather.",
+    ...config,
+    name: "fwf-v2",
+    slug: "fwf-v2",
+    version: '1.0.0',
+    extra: {
+        supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
+        supabaseKey: process.env.EXPO_PUBLIC_SUPABASE_KEY,
     },
-  },
-  android: {
-    ...config.android,
-    package: "com.willydennis.fairweatherfriends",
-    permissions: [
-      ...(config.android?.permissions || []),
-      "ACCESS_FINE_LOCATION",
-    ],
-  },
+    ios: {
+        ...config.ios,
+        bundleIdentifier: "com.willydennis.fwf",
+        supportsTablet: true,
+        infoPlist: {
+            NSContactsUsageDescription: "This app needs access to your contacts to help you connect with friends.",
+            NSLocationWhenInUseUsageDescription: "This app needs your location to show you the weather.",
+        },
+    },
+    android: {
+        ...config.android,
+        package: "com.willydennis.fairweatherfriends",
+        permissions: [
+            ...(config.android?.permissions || []),
+            "ACCESS_FINE_LOCATION",
+        ],
+    },
 });
