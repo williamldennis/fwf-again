12e5b848fa17bb87e4e0068e638982684c0bbd2d
Will Dennis <williamldennis@gmail.com>
2025-07-19 15:11:22 -0400

    version 1.0.4

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default ({ config }) => ({
     ...config,
     name: "fwf",
     slug: "fwf",
-    version: "1.0.3",
+    version: "1.0.4",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "fwf",
