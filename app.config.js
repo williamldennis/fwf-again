cd23180ede4e7abd9898d46caae637af5309ff82
Will Dennis <williamldennis@gmail.com>
2025-10-05 10:23:35 -0400

    pushing

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default ({ config }) => ({
     ...config,
     name: "fwf",
     slug: "fwf",
-    version: "1.0.2",
+    version: "1.0.11",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "fwf",
